215fa8fcb0173439d2cc5567690a97510b824513
Slawomir Czerwinski <slawomir@czerwinski.it>
2017-05-13 21:23:44 +0100

    Importing SharedPreferences.Editor

--- delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/_SharedPreferences.kt
+++ delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/_SharedPreferences.kt
@@ -1,12 +1,13 @@
 package it.czerwinski.android.delegates.sharedpreferences
 
 import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 
 fun SharedPreferences.getDouble(key: String, defaultValue: Double): Double =
 		if (contains(key)) java.lang.Double.longBitsToDouble(getLong(key, 0L))
 		else defaultValue
 
-fun SharedPreferences.Editor.putDouble(key: String, value: Double): SharedPreferences.Editor {
+fun Editor.putDouble(key: String, value: Double): Editor {
 	putLong(key, java.lang.Double.doubleToRawLongBits(value))
 	return this
 }
