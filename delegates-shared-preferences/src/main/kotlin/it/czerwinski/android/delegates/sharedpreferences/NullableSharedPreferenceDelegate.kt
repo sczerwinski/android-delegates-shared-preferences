23bacdf635d5ba3747a29c62af9c121bb2a14169
Slawomir Czerwinski <slawomir@czerwinski.it>
2017-05-13 21:25:57 +0100

    Limited scope of most properties in delegates
    Better names for parameters of the methods

--- delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/NullableSharedPreferenceDelegate.kt
+++ delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/NullableSharedPreferenceDelegate.kt
@@ -7,20 +7,20 @@ import kotlin.reflect.KProperty
 
 class NullableSharedPreferenceDelegate<T>(
 		context: Context,
-		val key: String,
-		val defaultValue: T,
-		val getter: SharedPreferences.(String, T) -> T,
-		val setter: SharedPreferences.Editor.(String, T) -> SharedPreferences.Editor) {
+		private val key: String,
+		private val defaultValue: T,
+		private val getter: SharedPreferences.(String, T) -> T,
+		private val setter: SharedPreferences.Editor.(String, T) -> SharedPreferences.Editor) {
 
 	private val sharedPreferences by lazy {
 		PreferenceManager.getDefaultSharedPreferences(context)
 	}
 
-	operator fun getValue(a: Any?, b: KProperty<*>): T? =
+	operator fun getValue(ref: Any?, property: KProperty<*>): T? =
 			if (sharedPreferences.contains(key)) sharedPreferences.getter(key, defaultValue)
 			else null
 
-	operator fun setValue(a: Any?, b: KProperty<*>, value: T?): Unit {
+	operator fun setValue(ref: Any?, property: KProperty<*>, value: T?): Unit {
 		val editor = sharedPreferences.edit()
 		if (value != null) editor.setter(key, value)
 		else editor.remove(key)
--- delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/SharedPreferenceDelegate.kt
+++ delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/SharedPreferenceDelegate.kt
@@ -7,19 +7,19 @@ import kotlin.reflect.KProperty
 
 class SharedPreferenceDelegate<T>(
 		context: Context,
-		val key: String,
-		val defaultValue: T,
-		val getter: SharedPreferences.(String, T) -> T,
-		val setter: SharedPreferences.Editor.(String, T) -> SharedPreferences.Editor) {
+		private val key: String,
+		private val defaultValue: T,
+		private val getter: SharedPreferences.(String, T) -> T,
+		private val setter: SharedPreferences.Editor.(String, T) -> SharedPreferences.Editor) {
 
 	private val sharedPreferences by lazy {
 		PreferenceManager.getDefaultSharedPreferences(context)
 	}
 
-	operator fun getValue(a: Any?, b: KProperty<*>): T =
+	operator fun getValue(ref: Any?, property: KProperty<*>): T =
 			sharedPreferences.getter(key, defaultValue)
 
-	operator fun setValue(a: Any?, b: KProperty<*>, value: T): Unit {
+	operator fun setValue(ref: Any?, property: KProperty<*>, value: T): Unit {
 		sharedPreferences.edit()
 				.setter(key, value)
 				.apply()
