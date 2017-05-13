293592648b4d98b52383505e89bd5e5c3da9d6b2
Slawomir Czerwinski <slawomir@czerwinski.it>
2017-05-13 14:15:09 +0100

    Created common file for factory functions

--- delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/_Delegates.kt
+++ delegates-shared-preferences/src/main/kotlin/it/czerwinski/android/delegates/sharedpreferences/_Delegates.kt
@@ -3,6 +3,18 @@ package it.czerwinski.android.delegates.sharedpreferences
 import android.content.Context
 import android.content.SharedPreferences
 
+fun Context.stringPreferenceDelegate(key: String) =
+		NullableSharedPreferenceDelegate<String>(
+				this, key, "",
+				SharedPreferences::getString,
+				SharedPreferences.Editor::putString)
+
+fun Context.stringPreferenceDelegate(key: String, defaultValue: String) =
+		SharedPreferenceDelegate<String>(
+				this, key, defaultValue,
+				SharedPreferences::getString,
+				SharedPreferences.Editor::putString)
+
 fun Context.intPreferenceDelegate(key: String) =
 		NullableSharedPreferenceDelegate<Int>(
 				this, key, 0,
